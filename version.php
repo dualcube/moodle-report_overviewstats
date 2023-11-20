efc1208275441d7771030a603acda68ab33f9e3d
Pravat Sahoo <pravats459@gmail.com>
2023-11-20 18:43:05 +0530

    #version-1.6.0 final-update

--- version.php
+++ version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_overviewstats';
 $plugin->release = 'v1.6.0';
-$plugin->version = 2023111600;
+$plugin->version = 2023112000;
 $plugin->requires = 2022041900; // Moodle 4.0.
 $plugin->maturity = MATURITY_STABLE;
