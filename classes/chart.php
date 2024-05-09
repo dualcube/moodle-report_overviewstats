47c3ee3b1bd301666dff9c1783cdb28f089d68a8
jayanta <jayantabarik7022@gmail.com>
2024-05-09 16:48:02 +0530

    version change

--- classes/chart.php
+++ classes/chart.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
  /**
-  * pluggin overviewstats
+  * plugin overviewstats
   *
   * @package report_overviewstats
   * @author DualCube <admin@dualcube.com>
