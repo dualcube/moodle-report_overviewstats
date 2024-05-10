20fbcb1d5a10247f61d04dc32e66861a0970ed78
jayanta <jayantabarik7022@gmail.com>
2024-05-10 13:52:52 +0530

    version update

--- version.php
+++ version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_overviewstats';
 $plugin->release = 'v1.6.1';
-$plugin->version = 2024050902;
+$plugin->version = 2024051000;
 $plugin->requires = 2022041900; // Moodle 4.0.
 $plugin->maturity = MATURITY_STABLE;
