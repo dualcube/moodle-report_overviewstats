b277a7192eff0fe8746e10e0b9cc26d0b60090f5
Purnendu Dash <32759662+purnendudash@users.noreply.github.com>
2025-01-09 16:44:07 +0530

    Update version.php

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_overviewstats';
-$plugin->release = 'v1.6.1';
+$plugin->release = 'v1.6.2';
 $plugin->version = 2025010900;
 $plugin->requires = 2022112815; // Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
